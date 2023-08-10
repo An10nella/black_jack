5ffbb69364f97a8d3fc84af36114f36a40c2d3eb
An10nella <135998311+An10nella@users.noreply.github.com>
2023-08-11 00:02:43 +0300

    create button function in js

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+let gameBtn = document.getElementById("inGame")
+let startBtnEl = document.getElementById("startBtn")
+let newBtnEl = document.getElementById("newBtn")
+let resetBtnEl = document.getElementById("resetBtn")
