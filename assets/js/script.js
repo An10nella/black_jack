1722bda8b55772e149315f8e448c5578d44a7827
An10nella <135998311+An10nella@users.noreply.github.com>
2023-08-11 01:30:19 +0300

    edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,39 @@ let gameBtn = document.getElementById("playCards")
 let startBtnEl = document.getElementById("startBtn")
 let newBtnEl = document.getElementById("newBtn")
 let resetBtnEl = document.getElementById("resetBtn")
+let sumScoreEL = document.getElementById("sumScore")
+let questPlay = document.getElementById("playQuestion")
 
 const cards = [1,2,3,4,5,6,7,8,9,10,11,13]
 
 startBtnEl.addEventListener("click",function(){
     let card01 = Math.floor(Math.random(cards)*cards.length)
+    // let card02 = Math.floor(Math.random()*13)+1
     let card02 = Math.floor(Math.random(cards)*cards.length)
+    
+
+    // let difference = 0
+    gameBtn.innerHTML  = `Cards: ${cards[card01]} ${cards[card02]}`
+    // gameBtn.innerHTML = `Cards : ${cards[card01]} ${cards[card02]} ${cards[card01]} `
+    sumScoreEL.innerHTML = ` Sum:${ cards[card01] + cards[card02]}`
+    // console.log(card02)
     // console.log(card02)
 
-    // console.log(card01)
-    // let difference = 0
-    gameBtn.innerHTML = `Cards: ${cards[card01]} ${cards[card02]}`
-    gameBtn.innerHTML = `Cards : ${cards[card01]} ${cards[card02]} ${cards[card01]} `
+   
+
+    // if (sumScoreEL.innerHTML == 21)
+    // questPlay.innerHTML = `is the winner`
+
   
    
 }
 )
+
+newBtnEl.addEventListener("click",function(){
+    gameBtn.innerHTML  = `Cards: ${cards[card01]} ${cards[card02]}`
+
+})
+
+resetBtnEl.addEventListener("click",function(){
+
+})
