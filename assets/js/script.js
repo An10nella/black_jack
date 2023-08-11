da36347723459599ad7f22502b3798750ac02fe3
An10nella <135998311+An10nella@users.noreply.github.com>
2023-08-11 11:17:48 +0300

    startgame btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ let newBtnEl = document.getElementById("newBtn")
 let resetBtnEl = document.getElementById("resetBtn")
 let sumScoreEL = document.getElementById("sumScore")
 let questPlay = document.getElementById("playQuestion")
+let sumCount = 0
+
 
 const cards = [10,11]
 let win = 21
@@ -12,20 +14,25 @@ startBtnEl.addEventListener("click",function(){
     // let card02 = Math.floor(Math.random()*13)+1
     let card02 = Math.floor(Math.random(cards)*cards.length)
     
+    sumCount = cards[card01] + cards [card02] + Math.floor(Math.random() * 10) + 1
+    console.log(su)
 
     // let difference = 0
-    gameBtn.innerHTML  = `Cards: ${cards[card01]} ${cards[card02]}`
-    sumScoreEL.innerHTML = ` Sum:${ cards[card01] + cards[card02]}`
+    gameBtn.innerHTML  = `Cards: ${cards[card01]} ${cards[card02]} `
+    sumScoreEL.innerHTML = ` Sum:${ sumCount}`
 
-    for(let i=0 ; i>cards.length ; i++){
-        if (sumScoreEL == win){
-            questPlay.innerHTML += `is the winner`
-            console.log( questPlay)
+
+        if (sumScoreEL.innerHTML == win){
+            questPlay.innerText = `Congratulations , You win!`
+            
+            }else {
+                questPlay.innerText = `repeat`
+
             }
         
         // gameBtn.innerHTML  = `Cards: ${cards[card01]} ${cards[card02]} `
 
-    }
+   
     // gameBtn.innerHTML = `Cards : ${cards[card01]} ${cards[card02]} ${cards[card01]} `
     
     // console.log(card02)
@@ -40,10 +47,18 @@ startBtnEl.addEventListener("click",function(){
 )
 
 newBtnEl.addEventListener("click",function(){
-    gameBtn.innerHTML  = `Cards: ${cards[card01]} ${cards[card02]}`
+    gameBtn.innerHTML = `Cards: Cards: ${cards[card01]} ${cards[card02] + Math.floor(Math.random() * 10) + 1} `
+    sumScoreEL.innerHTML = `Sum:`
+    questPlay.innerText = `Want to play a round?`
 
 })
 
 resetBtnEl.addEventListener("click",function(){
+    sumCount = 0
+    gameBtn.innerHTML = `Cards:`
+    sumScoreEL.innerHTML =`Sum:`
+    questPlay.innerText = `play?`
+    // resetBtnEl.cardsCount = 
+
 
 })
