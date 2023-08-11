c5e5652990ef053911355b1533626095b07acc4e
An10nella <135998311+An10nella@users.noreply.github.com>
2023-08-11 22:56:22 +0300

    all condition are correct as per game

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,25 +20,25 @@ startBtnEl.addEventListener("click", function () {
         oneTrial = true 
         balanceAdmin -= 50
 
-        let card01 = Math.floor(Math.random() * 11) + 1;
-        let card02 = Math.floor(Math.random() * 11) + 1;
+        let card01 = Math.floor(Math.random() * 13) + 1
+        let card02 = Math.floor(Math.random() * 13) + 1
         sumCount = card01 + card02;
 
-        bNameEl.innerHTML = `Antonella: $${balanceAdmin}`;
-        gameBtn.innerHTML = `Cards: ${card01} ${card02}`;
-        sumScoreEL.innerHTML = `Sum: ${sumCount}`;
+        bNameEl.innerHTML = `Antonella: $${balanceAdmin}`
+        gameBtn.innerHTML = `Cards: ${card01} ${card02}`
+        sumScoreEL.innerHTML = `Sum: ${sumCount}`
 
-        // if (sumCount === win) {
-        //     questPlay.innerText = "Congratulations, You win!";
-        // } else {
-        //     questPlay.innerText = "Try again?";
-        // }
+        if (sumCount == win) {
+            questPlay.innerText = "Congratulations, You win!"
+        } else {
+            questPlay.innerText = "Try again?"
+        }
     } else {
-        questPlay.innerText = "Cannot start another trial.";
+        questPlay.innerText = "Cannot start another trial."
     }
 })
 newBtnEl.addEventListener("click", function () {
-    if (balanceAdmin >= 50) {
+    if (balanceAdmin >= 50 && !maxNumb) {
         balanceAdmin -= 50
         bNameEl.textContent = `Antonella: $${balanceAdmin}`
        
@@ -49,22 +49,23 @@ newBtnEl.addEventListener("click", function () {
 
     gameBtn.innerHTML += ` ${randomCard}`
     sumScoreEL.innerHTML = ` Sum:  ${randCount} `
-   
-    if (randCount > win) {
-        questPlay.innerText = "You have exceeded the number required!"
-        maxNumb = true
 
-    } else if (randCount == win) {
-        questPlay.innerText = "Congratulations, You win!"  
+        if (randCount > win) {
+            maxNumb = true
+            questPlay.innerText = "You have exceeded the number required!"
+            // newBtnEl.disabled = true
+            return
+         } else if (randCount == win && !maxNumb) {
+            maxNumb = true
+            questPlay.innerText = "Congratulations, You win!"
         } else {
             questPlay.innerText = "Want to add more?"
         }
-
-} else  {
+    } else if (!maxNumb) {
+    questPlay.innerText = "Insufficient balance!"
+    } else {
     questPlay.innerText = "You have exceeded the number required!"
-    maxNumb = true
-}
-
+    }
   })
 
 resetBtnEl.addEventListener("click",function(){
@@ -72,6 +73,7 @@ resetBtnEl.addEventListener("click",function(){
     sumCount = 0
     cardsCount = 0
     oneTrial = false
+    maxNumb = false
     balanceAdmin = 200
     bNameEl.textContent = `Antonella: $${balanceAdmin}`
     gameBtn.innerHTML = 'Cards:'
